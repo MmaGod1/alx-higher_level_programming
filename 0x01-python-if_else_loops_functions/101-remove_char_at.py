2b8fcfb6e4f03effe1f8c7cba9c6daaa991a780a
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-04 04:43:15 -0800

    indentation fix

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    new_string = ""
     for i in range(len(string)):
         if i != n:
             new_string += string[i]
-        return new_string
+    return new_string
