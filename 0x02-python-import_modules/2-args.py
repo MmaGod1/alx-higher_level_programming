20eef7778454d2b26be4e31ce847080166fd12c1
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-04 10:34:06 -0800

    fix identation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(argc))
 
-        for i in range(1, argc + 1):
-            print("{}: {}".format(i, sys.argv[i]))i
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, sys.argv[i]))i
