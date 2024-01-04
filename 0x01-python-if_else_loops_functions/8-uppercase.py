f36ce86bb188da584f42491f5fa33fe136037c52
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-04 03:55:30 -0800

    logic changed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result = ""
-    for char in str:
-        if ord('a') <= ord(char) <= ord('z'):
-            result += chr(ord(char) - 32)
+    new_string = ""
+    for i in range(len(s)):
+        if 'a' <= s[i] <= 'z':
+            new_string += chr(ord(s[i]) - (ord('a') - ord('A')))
         else:
-            result += char
-        print(result)
+            new_string += s[i]
+        print("{}".format(new_string), end='')
