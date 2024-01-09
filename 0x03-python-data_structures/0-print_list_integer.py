06d32cd41a6151bcd0ad6172f6a12d69be18eeb0
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-09 02:05:14 -0800

    python list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[])
-print(my_list)
+
+# Using the * operator to unpack the list and pass it as arguments to format()
+print("List elements: {}".format(*my_list))
