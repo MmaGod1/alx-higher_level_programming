6083fe9b24d9181194a4b1893404b0a1963a20ef
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-09 02:08:47 -0800

    pycodestyle check

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[])
 
+
 # Using the * operator to unpack the list and pass it as arguments to format()
 print("List elements: {}".format(*my_list))
