b9a20d0664a987a52a106f6e2e4f8a95cfb76f99
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-09 05:44:05 -0800

    check if matrix exist

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
+    if not matrix:
+        return
+    else:
         for row in matrix:
             for col in row:
                 if col != row[-1]:
