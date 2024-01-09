aad9b3fa1815e39476476ddf2de2f988bdcac952
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-09 06:06:10 -0800

    changed logic

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
     for row in matrix:
-        for index, col in enumerate(row):
-            if index != len(row) - 1:
+        for col in row:
+            if col != row[-1]:
                 print("{:d}".format(col), end=" ")
             else:
-                print("{:d}".format(col))
+                print("{:d}".format(col), end='')
+            print()
