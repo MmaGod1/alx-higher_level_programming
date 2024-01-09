6e786081290820a9a1d07925cfec7695bf24c042
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-09 07:45:58 -0800

    logic changed

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence = "":
+    if sentence == "":
         return (0, None)
-    new_tuple = (len(sentence), sentence[0])
+    else:
+        new_tuple = (len(sentence), sentence[0])
     return new_tuple
