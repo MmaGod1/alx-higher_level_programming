1639e3add376d0799c235e527ca73dd5e48b6fb7
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-23 06:48:51 -0800

    exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -11,4 +11,5 @@ def safe_print_division(a, b):
     else:
         return result
     finally:
-        print("Inside result: {}".format(result) if 'result' in locals() else "No result available")
+        print("Inside result: {}".format(result) if 'result' in locals()
+              else "No result available")
