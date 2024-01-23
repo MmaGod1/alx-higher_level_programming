c8306cde6364af08047d1de5dd307a03677a7e4a
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-23 06:29:19 -0800

    exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Error: Division by zero")
+        return None
+    except TypeError:
+        print("Error: Both inputs must be integers")
+        return None
+    else:
+        return result
+    finally:
+        print("Inside result: {}".format(result) if 'result' in locals() else "No result available")
