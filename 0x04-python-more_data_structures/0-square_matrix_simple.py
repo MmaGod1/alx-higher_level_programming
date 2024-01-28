a86687811c1ec6cb5ca4aed9aa66677b429c9839
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-28 08:51:56 -0800

    data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,6 +5,6 @@ def square_matrix_simple(matrix=[]):
         squared_row = []
         for element in row:
             squared_row.append(element ** 2)
-            squared_matrix.append(squared_row)
+        squared_matrix.append(squared_row)
 
     return squared_matrix
