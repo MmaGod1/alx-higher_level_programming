86757cd92b2c6255108bddfc1a8cfa6efc21ab54
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-04 20:03:33 +0100

    Create 5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines a function to print text with indentation."""
+
+
+def text_indentation(text):
+    """
+    Print text with indentation.
+
+    Args:
+        text (str): The text to be printed.
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    # Define characters to split the text
+    split_chars = ['.', '?', ':']
+
+    # Initialize an empty line
+    new_line = ""
+
+    # Print text with indentation
+    for char in text:
+        new_line += char
+        if char in split_chars:
+            print(new_line.strip())  # Remove leading and trailing whitespaces
+            print()  # Print two empty lines
+            new_line = ""  # Reset the line
