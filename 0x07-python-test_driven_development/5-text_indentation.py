aab146a55ac9ff51970ce7c493439c6cda4e0699
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-05 00:17:36 +0100

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,22 +10,28 @@ def text_indentation(text):
     Raises:
         TypeError: If text is not a string.
     """
-    # Check if text is a string
-    if type(text) is not str:
-        raise TypeError("Input must be a string")
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
 
     index = 0
 
-    # Skip leading spaces
-    while index < len(text) and text[index] == ' ':
-        index += 1
-
     while index < len(text):
+        # Skip leading spaces
+        while index < len(text) and text[index] == ' ':
+            index += 1
+
+        # Check if we've reached the end of the text
+        if index == len(text):
+            break
+
         print(text[index], end='')
 
+        # Check for newline or specified punctuation marks
         if text[index] in ['.', '?', ':']:
             print('\n\n', end='')
+            index += 1
         elif text[index] == '\n':
-            # Ensure no space at the beginning of the line after newline
             print("", end='')
-        index += 1
+            index += 1
+        else:
+            index += 1
