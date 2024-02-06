b713ccb9886b8a7436dfd7d98552794269faeac3
MmaGod1 <gmmaduchukwu@gmail.com>
2024-02-06 12:21:58 -0800

    Test

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,7 +3,7 @@
 
 
 def is_kind_of_class(obj, a_class):
-  """Check if an object is exactly an instance of a given class.
+    """Check if an object is exactly an instance of a given class.
 
     Args:
         obj (any): The object to check.
