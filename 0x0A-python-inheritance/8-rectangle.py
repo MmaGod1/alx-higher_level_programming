63d6c4b8391c0b6671aa3f452b346878b891197f
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-07 13:27:30 +0100

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -36,5 +36,7 @@ class Rectangle(BaseGeometry):
             height (int): The height of the new Rectangle.
         """
         super().__init__()
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
