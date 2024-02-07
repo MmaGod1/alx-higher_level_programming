ab613deeca94aed4b7deb6413c29685403a4b6c3
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-07 05:19:52 +0100

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -28,7 +28,7 @@ class Student:
         """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
         return self.__dict__
 
     def reload_from_json(self, json):
@@ -37,5 +37,5 @@ class Student:
         Args:
             json (dict): The key/value pairs to replace attributes with.
         """
-        for k, v in json.items():
-            setattr(self, k, v)
+        for key, value in json.items():
+            setattr(self, key, value)
