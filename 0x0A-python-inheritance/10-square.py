f3d52b5f267cbfbb808753e3ca6a0101e11ff2e9
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-07 14:16:27 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines a base geometry class BaseGeometry."""
-
+Rectangle = __import__('9-rectangle').Rectangle
 
 class BaseGeometry:
     """Represent base geometry."""
@@ -59,5 +59,6 @@ class Square(Rectangle):
         Args:
             size (int): The size of the new square.
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
