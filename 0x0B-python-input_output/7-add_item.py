dfa113c6d4cb3a7db23cefa9da95b3d90e0d9ac3
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-07 05:28:03 +0100

    Update 7-add_item.py
    Import file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+from save_to_json_file import 5-save_to_json_file
+from load_from_json_file import 6-load_from_json_file
 
 if __name__ == "__main__":
     try:
