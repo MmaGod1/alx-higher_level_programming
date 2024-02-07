25d5719c80a36d66d0866b92686b2ac9a5337f54
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-07 13:56:45 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -36,17 +36,17 @@ class Rectangle(BaseGeometry):
             height (int): The height of the new Rectangle.
         """
         super().__init__()
-        self.__width = 0
-        self.__height = 0
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
+        """Calculate the area of the Rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
+        """Return the string representation of the Rectangle."""
         return f"[Rectangle] {self.__width}/{self.__height}"
 
 
