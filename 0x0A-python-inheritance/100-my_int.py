6ee4058c45f9228926a24e4b89659ba25801b987
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-07 14:57:51 +0100

    Create 100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    """Class MyInt that inherits from int."""
+
+    def __eq__(self, other):
+        """Override the == operator."""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Override the != operator."""
+        return super().__eq__(other)
