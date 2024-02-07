c5dd953ae60238f7b88ee8b3ce2e03fed2746418
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-07 14:24:04 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,53 +2,6 @@
 """Defines a base geometry class BaseGeometry."""
 Rectangle = __import__('9-rectangle').Rectangle
 
-class BaseGeometry:
-    """Represent base geometry."""
-
-    def area(self):
-        """Not yet implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """Represent a rectangle using BaseGeometry."""
-
-    def __init__(self, width, height):
-        """Intialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-        """
-        super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Calculate the area of the Rectangle."""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Return the string representation of the Rectangle."""
-        return f"[Rectangle] {self.__width}/{self.__height}"
-
 
 class Square(Rectangle):
     """Represent a square."""
