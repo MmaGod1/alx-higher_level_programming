c324510fb896d8aab4a58a80eb932cb7500ef96e
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-07 05:10:26 +0100

    Update 11-student.py
    Update methods

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -26,11 +26,10 @@ class Student:
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
-      if attrs is None:
-            return self.__dict__
-        else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
-
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
     def reload_from_json(self, json):
         """
         Replaces all attributes of the Student instance with values from a dictionary.
