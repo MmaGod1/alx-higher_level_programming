b820ea01b196373414328b197be636c83075a8cb
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-13 03:46:06 +0100

    Update square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,27 @@ class Square(Rectangle):
         """Initialize Square instance."""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter method for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter method for size."""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Assign attributes to instance."""
+        if args:
+            attrs = ['id', 'size', 'x', 'y']
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
         """Override __str__ method to return formatted string."""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
