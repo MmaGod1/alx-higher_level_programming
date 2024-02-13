23e7e563ebad791fe384d95196cfdfc7cdc47180
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-02-13 04:08:50 +0100

    Update base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Module for Base class."""
 import json
 
+
 class Base:
     """Base class for managing id attribute."""
 
@@ -14,7 +15,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-            
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return JSON string representation of list_dictionaries."""
@@ -31,3 +32,22 @@ class Base:
         filename = cls.__name__ + ".json"
         with open(filename, 'w') as file:
             file.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return list of dictionaries from JSON string representation."""
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Create instance with attributes set from dictionary."""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
