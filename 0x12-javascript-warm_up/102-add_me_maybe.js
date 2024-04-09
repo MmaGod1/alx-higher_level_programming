5aba86fe35c8f9cd73823f285971eab9161456a4
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-04-09 23:12:52 +0100

    Update 102-add_me_maybe.js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-function callMeMoby (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
-  }
-}
-
-module.exports.callMeMoby = callMeMoby;
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
+};
