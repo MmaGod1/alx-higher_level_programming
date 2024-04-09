317c36597a4a9732d839ccad6acde39f399555e8
MmaGod1 <gmmaduchukwu@gmail.com>
2024-04-09 06:11:56 -0700

    semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function factorial(n) {
+function factorial (n) {
   if (isNaN(n)) {
     return 1;
   }
-  
+
   if (n === 0 || n === 1) {
     return 1;
   } else {
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const multiLine = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-for (let line of multiLine) {
+const multiLine = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line of multiLine) {
   console.log(line);
 }
