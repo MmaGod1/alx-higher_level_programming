1564dfaba30ed683e6b0838fcb642ec16c7115ae
MmaGod1 <gmmaduchukwu@gmail.com>
2024-04-16 00:02:43 -0700

    pycodestyle check

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,13 +11,15 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State)\
+        .filter(State.name.like('%a%')).order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
