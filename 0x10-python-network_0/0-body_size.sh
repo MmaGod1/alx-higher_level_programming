86ecd3a37190b1aa8a97b2d18d20ea6cdb570307
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-04-25 15:03:58 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-# a script that takes in a URL, sends a request to
-# that URL, and displays the size of the body of the response
+#!/bin/bash
+# a script that takes in a URL, sends a request, and displays the size of the response
 curl -s "$1" | wc -c
