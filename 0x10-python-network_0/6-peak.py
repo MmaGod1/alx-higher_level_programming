c2ba06dd13cd157874018f37b8ee1939799369ff
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-04-25 16:08:13 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function to find a peak in an unsorted list of integers."""
 
+
 def find_peak(int_list):
     """Finds a peak in the given list of integers."""
 
@@ -9,7 +10,7 @@ def find_peak(int_list):
 
     low = 0
     high = len(int_list) - 1
- 
+
     while low < high:
         mid = (low + high) // 2
 
