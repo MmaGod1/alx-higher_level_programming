f43f02aaff6dff77942283b81a5158c857f78be2
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-04-26 20:16:48 +0100

    Create 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module uses the GitHub API to display the user's id using Basic Authentication with a personal access token.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+    
+    if response.status_code == 200:
+        data = response.json()
+        user_id = data.get('id')
+        print(user_id)
+    else:
+        print("Error:", response.status_code)
