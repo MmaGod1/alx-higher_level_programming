e75b4af8420ebcfa91484e37594d257bd9724f1c
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-20 14:08:12 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const x = parseInt(process.argv);
-  for (let i = 0; i < x; i++) {
-if (IsNaN(x[i])) {
+const x = parseInt(process.argv[2]);
+if (IsNaN(x)) {
     console.log('Missing number of occurrences');
-  }
-} else {
+  } else {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
+  }
 }
+  
