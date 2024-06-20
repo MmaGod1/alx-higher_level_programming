77fe044f6adfe52e185614852a53119ae0bba3ff
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-20 13:52:34 +0100

    Different implementation

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
-if (!isNaN(x)) {
+const x = parseInt(process.argv);
   for (let i = 0; i < x; i++) {
-    console.log('C is fun');
+if (IsNaN(x[i])) {
+    console.log('Missing number of occurrences');
   }
 } else {
-  console.log('Missing number of occurrences');
+    console.log('C is fun');
 }
