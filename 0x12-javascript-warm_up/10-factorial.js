31bd165658dcd28b5d005bfbe2ec22a5d7298303
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-20 18:12:08 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+const arg = parseInt(process.argv[2])
 function factorial (n) {
   if (isNaN(n)) {
     return 1;
@@ -11,4 +12,4 @@ function factorial (n) {
   }
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+console.log(factorial(arg));
