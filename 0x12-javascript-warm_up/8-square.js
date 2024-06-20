5ef90ec10694381f29de57acedd000e012c3d71c
MmaGod1 <gmmaduchukwu@gmail.com>
2024-06-20 09:46:11 -0700

    semistandard fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,6 @@
 const size = parseInt(process.argv[2]);
 if (isNaN(size)) {
   console.log('Missing size');
-  }
 } else {
   for (let i = 0; i < size; i++) {
     let row = '';
@@ -10,4 +9,5 @@ if (isNaN(size)) {
       row += 'X';
     }
     console.log(row);
+  }
 }
