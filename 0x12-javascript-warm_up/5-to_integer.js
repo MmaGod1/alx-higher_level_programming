e9f9b0f5b7e4d75206d94ed132ffa4474759763e
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-20 09:04:02 +0100

    Logic changed

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const arg = process.argv[2];
 const number = parseInt(arg);
-if (!isNaN(number)) {
-  console.log('My number:', number);
-} else {
+if (isNaN(number)) {
   console.log('Not a number');
+} else {
+  console.log('My number:', number);
 }
