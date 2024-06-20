2a3474d88849b4e6516d7338c489c571e00aad04
MmaGod1 <gmmaduchukwu@gmail.com>
2024-06-20 06:11:43 -0700

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const x = parseInt(process.argv);
-  for (let i = 0; i < x; i++) {
-if (IsNaN(x[i])) {
-    console.log('Missing number of occurrences');
+  for (let i = 0; i < x[2]; i++) {
+	  if (IsNaN(x[2])) {
+		  console.log('Missing number of occurrences');
+	  } else {
+		  console.log('C is fun');
+	  }
   }
-} else {
-    console.log('C is fun');
-}
