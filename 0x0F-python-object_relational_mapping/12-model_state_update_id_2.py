067ecc7128be9b35919b5fb1667b8e8e98766a10
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-25 04:09:08 +0100

    Update 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_instance = session.query(State).filter_by(id=2).first()
-    new_instance.name = 'New Mexico'
+    update_state = session.query(State).filter_by(id=2).first()
+    update_state.name = 'New Mexico'
     session.commit()
+    Session.close()
