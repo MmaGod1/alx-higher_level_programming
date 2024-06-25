2d8cf1f5b698850f9c573106bd4d9e659997b1d9
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-25 04:26:53 +0100

    Update 13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        session.delete(instance)
+    for del_state in session.query(State).filter(State.name.like('%a%')):
+        session.delete(del_state)
     session.commit()
+    session.close()
